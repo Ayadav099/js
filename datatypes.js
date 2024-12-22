18b5c5c2b884118903729581b02ae375ba7f6dcc
Ayadav099 <amaryadaw903@gmail.com>
2024-12-22 16:03:33 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+/// data types in js
+
+console.log('datatypes')
+
+
+let name = "amar"
+let age  = 22
+let isloggedin= false
+let state ; //value not asigned
+
+//number
+//bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+//undefined =>
+// symbol  => unique
+
+//object
+
+console.log(typeof undefined)//undefined
+
+console.log(typeof null)// object
