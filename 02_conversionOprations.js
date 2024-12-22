98983034db029219a6441b7081992ef7dccd37ff
Ayadav099 <amaryadaw903@gmail.com>
2024-12-22 18:47:03 +0530

    opreations

---
+++ 02_conversionOprations.js
@@ -0,0 +1,20 @@
+/// conversions Opeartions
+
+
+let  str1 = " hello"
+
+let str2 = " Amar"
+
+console.log(str1+ str2)
+console.log( 1 + "2")
+console.log("1" + 2)
+
+console.log("1"+ 1+ 2)
+
+
+    let one =1
+    let two= "1"
+    
+       if(one = two){
+        console.log( true)
+       }
