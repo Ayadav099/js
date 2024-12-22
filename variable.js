e9c85ef26d7a6feeec7e07d2580c779e47124f85
Ayadav099 <amaryadaw903@gmail.com>
2024-12-22 15:37:54 +0530

    variablesin js

---
+++ variable.js
@@ -0,0 +1,25 @@
+
+const account_Id=14553
+
+let accountEmail= "ayadav9092gmail.com"
+
+
+var accountPassword= "12456"
+
+accountcity="Mohali"
+
+//account_Id =2   cannot assign value in const varibale
+console.log(account_Id)
+
+accountEmail="yadav878787@gmail.com";
+
+console.log(accountEmail);
+
+console.table([account_Id,accountEmail,accountPassword,accountcity])
+
+
+
+
+
+
+
