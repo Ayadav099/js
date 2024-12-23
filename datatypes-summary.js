616fef533c95f40adf0c7cceb4f51a9691c192da
Ayadav099 <amaryadaw903@gmail.com>
2024-12-23 21:08:33 +0530

    datatypes

---
+++ datatypes-summary.js
@@ -0,0 +1,33 @@
+// primitive 
+
+// 7 types =>  string ,Number,Boolean,number,undefined ,symbol,BIGINT
+
+
+// Refrence ( Non primitive)
+
+// Arrays,objects,functions
+
+
+//Q 1. js is dynamically or statically type
+ // ans == dynamically type 
+
+
+
+
+const score = 100;
+const scoreval=100.3;
+const loggedin=false;
+const outsidetemp=null;
+
+let email;
+
+//symbol
+
+const id = Symbol('123')
+const anotherId=Symbol('123');
+
+console.log('symbolinpt', id===anotherId)
+
+
+
+// 2. reftype (non
