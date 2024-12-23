7fd80b3264378cf6f2312c5c015fb60bcc0f604d
Ayadav099 <amaryadaw903@gmail.com>
2024-12-23 20:53:55 +0530

    comparison

---
+++ 03comaprison.js
@@ -0,0 +1,29 @@
+/// conversions Opeartions
+
+
+let  str1 = " hello"
+
+let str2 = " Amar"
+
+console.log(str1+ str2)
+console.log( 1 + "2")
+console.log("1" + 2)
+
+console.log("1"+ 1+ 2)
+
+
+    let one =1
+    let two= "1"
+    
+       if(one = two){
+        console.log( true)
+       }
+//
+
+// console.log( "2" > 1)
+// console.log( "02" > 1)         /// avoid these kind of comparison
+
+console.log(null > 0)
+
+console.log(null == 0)
+console.log(null >= 0)
