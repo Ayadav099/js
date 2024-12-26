4d01bcc782e86137de2c5a07e27b5cbbd5f89db8
Ayadav099 <amaryadaw903@gmail.com>
2024-12-26 21:55:37 +0530

    Numbers

---
+++ 06_Number.js
@@ -0,0 +1,53 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+
+// Numbers and maths
+
+ const score = 400
+ 
+ console.log(score);
+ 
+ 
+ const balance = new Number(100)
+ console.log(balance)
+
+console.log(balance.toString())
+
+console.log(balance.toString().length)
+
+
+console.log(balance.toFixed(1))
+
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4))
+
+
+
+
+const Hundreds = 100000
+
+console.log(Hundreds.toLocaleString())// us standrd
+
+console.log(Hundreds.toLocaleString('en-IN'))// Indian standrd
+
+
+/// MAths (lerts talk about maths)
+
+console.log(Math)
+console.log(Math.abs(-4)) //positive remain positive neg changes positve
+
+//round
+console.log(Math.round(4.6))
+
+console.log(Math.floor(4.6)) // lowest value
+
+console.log(Math.ceil(4.6)) // top value
+
+
+console.log(Math.min(4,3,4,5,6,7,6)) // 
+
+console.log(Math.max(4,3,4,5,6,7,6))
