48fdc1318acc8d8dd9374558ace3f805ec0a0dd1
Ayadav099 <amaryadaw903@gmail.com>
2024-12-29 19:39:23 +0530

    datetime

---
+++ dateandtime.js
@@ -0,0 +1,25 @@
+
+
+/// date and time 
+
+//===================date time in depth in js
+
+
+
+let myDate = new Date()
+
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate)
+
+
+// new specifice date
+
+let myCreatedDate = new Date ( 2023 ,0 ,25)
+console.log(myCreatedDate)
+
+console.log(myCreatedDate.toDateString())
+
